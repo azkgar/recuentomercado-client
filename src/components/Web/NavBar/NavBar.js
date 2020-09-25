b492a98e90967755909ae7f0c4822627d0d64a9e
Azkary Garcia <agarcia@aparente.mx>
2020-09-24 23:17:33 -0500

    Animates home menu

--- src/components/Web/NavBar/NavBar.js
+++ src/components/Web/NavBar/NavBar.js
@@ -1,14 +1,26 @@
 import React, {useState, useEffect, useRef} from "react";
 import {Link} from "react-router-dom";
+import {Menu} from "antd";
 import {getMenuApi} from "../../../api/menu";
-import Menu from "../NavMenu";
 import Logo from "../../../assets/img/png/RM-logotipo_fondoblanco.png";
 
 import "./NavBar.scss";
 
 export default function NavBar(){
     const [isVisible, setIsVisible] = useState(false);
+    const [menuData, setMenuData] = useState([]);
     const node = useRef()
+
+    useEffect(()=>{
+        getMenuApi()
+        .then(response => {
+            const arrayMenu = [];
+            response.menu.forEach(item => {
+                item.active && arrayMenu.push(item);
+            });
+            setMenuData(arrayMenu);
+        })
+    },[]);
     
     function visible() {
         setIsVisible(!isVisible);
@@ -30,12 +42,10 @@ export default function NavBar(){
     }, []);
 
     const showClass = isVisible ? "show-menu" : "hide-menu";
+    const showText = isVisible ? "show-text" : "hide-text";
 
     return(
         <div className = "navbar">
-            <Menu
-                showClass = {showClass}
-            />
             <div className = "navbar__hamburguer-menu" ref = {node}>
                 <input
                     checked = {isVisible}
@@ -56,11 +66,48 @@ export default function NavBar(){
                     htmlFor = "toggle"
                     className = "bottom"
                 ></label>
+                <Menu
+                    className = {showClass}
+                    mode = "vertical"
+                >
+                    {menuData.map(item => {
+                        const external = item.url.indexOf("http") > -1 ? true : false;
+
+                        if(external) {
+                            return(
+                                <Menu.Item
+                                    key = {item._id}
+                                    className = {showText}
+                                    onClick = {visible}
+                                >
+                                    <a
+                                        href = {item.url}
+                                        target = "_blank"
+                                        rel = "noopener noreferrer"
+                                    >
+                                        {item.title}
+                                    </a>
+                                </Menu.Item>
+                            );
+                        }
+
+                        return(
+                            <Menu.Item
+                                key = {item._id}
+                                className = {showText}
+                                onClick = {visible}
+                            >
+                                <Link to = {item.url}>
+                                    {item.title}
+                                </Link>
+                            </Menu.Item>
+                        );
+                    })}
+                </Menu>
             </div>
             <a href = "/">
                 <img src = {Logo} alt = "Logo el Recuento del Mercado" />
             </a>
-            
         </div>
     );
 }
